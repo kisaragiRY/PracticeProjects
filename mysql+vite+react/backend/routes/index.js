8cf1b4a2eb5cea2eb1d5ccfcd9fc4243f572935f
kisaragi <azeuunghy@gmail.com>
2023-12-12 14:48:36 +0300

    added employee routes #1

--- mysql+vite+react/backend/routes/index.js
+++ mysql+vite+react/backend/routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+// employee routes
+const employeeRoutes = require("./employeeRoutes");
+router.use("/employee", employeeRoutes);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });

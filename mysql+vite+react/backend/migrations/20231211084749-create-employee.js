6e4645911e0f50987a51c11be6f4633f5a8075e4
kisaragi <azeuunghy@gmail.com>
2023-12-12 14:49:13 +0300

    deleted unwanted cols #1

--- mysql+vite+react/backend/migrations/20231211084749-create-employee.js
+++ mysql+vite+react/backend/migrations/20231211084749-create-employee.js
@@ -17,14 +17,6 @@ module.exports = {
       },
       dept: {
         type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },

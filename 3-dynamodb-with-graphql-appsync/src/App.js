fb88bf9b010ee11de917876508116f445127de9e
kisaragi <r_zhang@ga-tech.co.jp>
2024-01-09 10:44:05 +0900

    added new routes #4

--- 3-dynamodb-with-graphql-appsync/src/App.js
+++ 3-dynamodb-with-graphql-appsync/src/App.js
@@ -6,9 +6,10 @@ import './App.css';
 
 import { Home } from './pages/Home';
 
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+// import { Authenticator } from '@aws-amplify/ui-react';
+// import '@aws-amplify/ui-react/styles.css';
 
+import { LoginPage, RegisterPage, ValidatePage } from './components.js/auth';
 
 import { Amplify } from 'aws-amplify';
 import awsmobile from './aws-exports'
@@ -18,6 +19,9 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/'>
       <Route path='/' element={<Home/>} />
+      <Route path='/register' element={RegisterPage} />
+      <Route path='/login' element={LoginPage} />
+      <Route path='/validate' element={ValidatePage} />
     </Route>
   )
 )
